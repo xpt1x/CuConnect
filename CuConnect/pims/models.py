e8115b76897cf91267ef928eed3c48b5fd7a5b34
Vivek <viveksonybcd@gmail.com>
2021-01-25 17:01:25 +0530

    added models

--- CuConnect/pims/models.py
+++ CuConnect/pims/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def upload_image(self, filename, **kwargs):
+    return f'images/{str(self.name)}-{filename}'
+
+
+class Comment(models.Model):
+    msg = models.CharField(max_length=64, blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128, blank=False, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ManyToManyField(Comment)
+    likes = models.IntegerField(default=0, blank=False, null=False)
+    image = models.ImageField(upload_to=upload_image, blank=False, null=False,)
+# class Profile(models.Model):
+#     username = models.ForeignKey()
