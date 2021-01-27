cac5518205d4a64931e97e3a8eb162eeb9427b04
Jatin Sarda <jatinsarda007@gmail.com>
2021-01-27 15:28:54 +0530

    rep system logic

--- CuConnect/pims/models.py
+++ CuConnect/pims/models.py
@@ -1,21 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 def upload_post_image(self, filename, **kwargs):
-    return f'images/posts/{str(self.title)}-{filename}'
+    return f"images/posts/{str(self.title)}-{filename}"
+
 
 def upload_profile_image(self, filename, **kwargs):
-    return f'images/profiles/{str(self.user_id)}-{filename}'
-
+    return f"images/profiles/{str(self.user_id)}-{filename}"
 
 
 class Post(models.Model):
     title = models.CharField(max_length=128, blank=False, null=False)
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, related_name="posts")
-    likes = models.IntegerField(default=0, blank=False, null=False)
+    author = models.ForeignKey(
+        "UserProfile", on_delete=models.CASCADE, related_name="posts"
+    )
     image = models.ImageField(upload_to=upload_post_image, blank=False, null=False)
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
+
+    likes = models.IntegerField(default=0, blank=False, null=False)
+    reports = models.IntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
         return self.title
@@ -23,22 +28,32 @@ class Post(models.Model):
 
 class Comment(models.Model):
     msg = models.CharField(max_length=64, blank=False, null=False)
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    author = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self):
-        return f'{self.msg} -{self.author}'
-        
+        return f"{self.msg} - {self.author}"
+
+
 class UserProfile(models.Model):
     user_id = models.CharField(max_length=10, blank=False, null=False)
-    display_name = models.CharField(max_length=32, blank=False, null=False)   
+    display_name = models.CharField(max_length=32, blank=False, null=False)
     picture = models.ImageField(upload_to=upload_profile_image, blank=True, null=True)
+    rep = models.IntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
         return self.display_name
 
 
+@receiver(post_save, sender=Post)
+def increase_user_rep_on_post_create(
+    sender, update_fields, instance, created, **kwargs
+):
+    if created:
+        instance.author.rep += 5
+        instance.author.save()
+    else:
+        print("else")
 
-    
+    print(update_fields)
--- CuConnect/pims/serializers.py
+++ CuConnect/pims/serializers.py
@@ -5,15 +5,25 @@ from .models import Post, Comment, UserProfile
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'url','title', 'author', 'likes', 'image', 'timestamp', 'comments']
+        fields = [
+            "id",
+            "url",
+            "title",
+            "author",
+            "likes",
+            "image",
+            "timestamp",
+            "comments",
+        ]
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'url', 'msg', 'timestamp', 'author', 'post']
+        fields = ["id", "url", "msg", "timestamp", "author", "post"]
+
 
 class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ['id', 'url', 'display_name', 'user_id', 'posts']
+        fields = ["id", "url", "display_name", "user_id", "posts", "rep"]
