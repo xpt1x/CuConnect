1ef9b86da70db9202a8212c85c05d113063d7b3f
Swarnim Doegar <swarnimdoegar@gmail.com>
2021-03-28 23:36:52 +0530

    Pulled changes from xpt1x

--- frontend/mobx/stores/AttendanceStore.ts
+++ frontend/mobx/stores/AttendanceStore.ts
@@ -1,21 +1,4 @@
 import { action, makeAutoObservable } from "mobx";
-<<<<<<< HEAD
-import { min } from "react-native-reanimated";
-import {TimetableType} from "../../types/TimetableTypes"
-
-export default class TimeTableStore {
-  attendance = undefined
-  fullAttendance = undefined
-
-  @action.bound
-  setFullAttendance(fullAttendance) {
-    this.fullAttendance = fullAttendance;
-  }
-
-  @action.bound
-  setAttendance(minimalAttendance){
-    this.attendance = minimalAttendance;
-=======
 import { Subject } from "../../types/Subject";
 
 export default class AttendanceStore {
@@ -24,7 +7,6 @@ export default class AttendanceStore {
   @action.bound
   setAttendance(attendance: ReadonlyArray<Subject>){
     this.attendance = attendance;
->>>>>>> c394cde99882034204ed7ddf22b6044efd96d021
   }
 
   constructor() {
