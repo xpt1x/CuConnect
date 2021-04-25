5fd579d8d0f497c7f2c11ffad36696a5c5eae09d
Jatin Sarda <jatinsarda007@gmail.com>
2021-04-25 17:29:32 +0530

    added checkUser method

--- CuConnect/pims/urls.py
+++ CuConnect/pims/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet, UserProfileViewSet, get_marks
+from .views import PostViewSet, CommentViewSet, UserProfileViewSet, checkUser, get_marks
 
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet)
@@ -10,6 +10,7 @@ router.register("profiles", UserProfileViewSet)
 
 urlpatterns = [
     path("validate", views.validate),
+    path("checkuser", views.checkUser),
     path("register", views.register_user),
     path("attendance", views.get_minimal_attendance),
     path("fullattendance", views.get_full_attendance),
