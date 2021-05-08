f7beafecdcebb523b1bac89e4e14f788417c2848
Jatin Sarda <jatinsarda007@gmail.com>
2021-05-09 00:59:53 +0530

    updated DB

--- CuConnect/CuConnect/settings.py
+++ CuConnect/CuConnect/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = "CuConnect.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_DATABASE"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
