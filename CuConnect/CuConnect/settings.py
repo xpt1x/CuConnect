6b10d45f2f9acec8aae8dabc716a0a87e4bcfd50
Jatin Sarda <jatinsarda007@gmail.com>
2021-05-12 00:58:02 +0530

    Co-authored-by: Swarnim Doegar <SwarnimDoegar@users.noreply.github.com> Co-authored-by: Vivek Sony <Vtrrix@users.noreply.github.com> Co-authored-by: Shreyans Jain <shreyans1313@gmail.com>

--- CuConnect/CuConnect/settings.py
+++ CuConnect/CuConnect/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = "CuConnect.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_DATABASE"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.getenv("DB_DATABASE"),
+    #     "USER": os.getenv("DB_USER"),
+    #     "PASSWORD": os.getenv("DB_PASSWORD"),
+    #     "HOST": os.getenv("DB_HOST"),
+    #     "PORT": os.getenv("DB_PORT"),
+    # }
 }
 
 
